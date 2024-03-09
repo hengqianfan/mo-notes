5911cc83c658b1b54e6ca8f7a0795c288072bb37
hengqianfan <157280112+hengqianfan@users.noreply.github.com>
2024-03-09 20:18:55 +0800

    0.6.3
    .

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -23,7 +23,7 @@ export default defineConfig({
   // srcDir: 'docs',
   // lastUpdated: true,
   themeConfig: {
-    siteTitle: 'mo-notes-0.6.2',
+    siteTitle: 'mo-notes-0.6.3',
     logo: '/icon/png/logo.png',
     // https://vitepress.dev/reference/default-theme-config
     nav: [
